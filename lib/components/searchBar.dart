2a5be30a03b0047ae42e42d48a74f8b82e8b847b
Aurélien <aureliendu3809@outlook.fr>
2021-03-25 14:13:36 +0100

    search bar

---
+++ lib/components/searchBar.dart
@@ -0,0 +1,86 @@
+import 'package:dwm14/constants/firebase.dart';
+import 'package:dwm14/models/movie.dart';
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  TextEditingController _textController = TextEditingController();
+  static List moviesTitle = [];
+  List newDataList = List.from(moviesTitle);
+
+  getMovieTitle(moviesData) {
+    moviesTitle = [];
+    for (var i = 0; i < moviesData.length; i++) {
+      moviesTitle.add(moviesData[i].title);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getMovies(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(
+                child: SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            default:
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Text('Aucun film na été trouvé'),
+                );
+              }
+              List moviesData = snapshot.data;
+              getMovieTitle(moviesData);
+              return Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: 'Rechercher un film..',
+                      ),
+                      onChanged: (String value) {
+                        print(moviesTitle);
+                        setState(() {
+                          newDataList = moviesTitle
+                              .where((string) => string
+                                  .toLowerCase()
+                                  .contains(value.toLowerCase()))
+                              .toList();
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView(
+                      padding: EdgeInsets.all(12.0),
+                      children: newDataList.map((data) {
+                        return ListTile(
+                          title: Text(data),
+                          onTap: () => Navigator.pushNamed(context, '/detail',
+                              arguments: ItemArguments(title: data)),
+                        );
+                      }).toList(),
+                    ),
+                  )
+                ],
+              );
+          }
+        });
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:dwm14/components/drawer.dart';
 import 'package:dwm14/components/landing.dart';
+import 'package:dwm14/components/searchBar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,34 +12,38 @@ class _HomeScreenState extends State<HomeScreen> {
   int nb = 0;
   Color color = Colors.blue;
   bool disable = false;
-  String _selectedItemText = '';
-  List list = [
-    "Flutter",
-    "React",
-    "Ionic",
-    "Xamarin",
-  ];
-  //List<Widget> list = [Landing(), Container(), Text('School')];
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Flutter'),
-      ),
-      drawerEnableOpenDragGesture: !disable,
-      drawer: DrawerMenu(),
-      body: SafeArea(
-        child: Column(
-          children: [
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Flutter'),
+        ),
+        drawerEnableOpenDragGesture: !disable,
+        drawer: DrawerMenu(),
+        body: SafeArea(
+          child: Column(children: [
             Container(
               width: 500,
               height: 300,
               child: Landing(),
             ),
-          ],
-        ),
-      ),
-    );
+            Container(
+              height: 300,
+              child: SearchBar(),
+            ),
+          ]),
+        ));
+  }
+}
+
+class Test extends StatelessWidget {
+  const Test({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
   }
 }
