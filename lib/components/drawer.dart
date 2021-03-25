6ffc34a4397ee13b3de8e0aca01e8a6bebe19508
Aurélien <aureliendu3809@outlook.fr>
2021-03-25 18:56:54 +0100

    fix bug

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -9,6 +9,12 @@ class DrawerMenu extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           ListTile(
+            title: Text('Accueil'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/home');
+            },
+          ),
+          ListTile(
             title: Text('Profile'),
             onTap: () {
               Navigator.pushReplacementNamed(context, '/profile');
---
+++ lib/screens/category.dart
@@ -0,0 +1,49 @@
+import 'package:dwm14/components/drawer.dart';
+import 'package:dwm14/components/landing.dart';
+import 'package:dwm14/components/searchBar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int nb = 0;
+  Color color = Colors.blue;
+  bool disable = false;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Flutter'),
+        ),
+        drawerEnableOpenDragGesture: !disable,
+        drawer: DrawerMenu(),
+        body: SafeArea(
+          child: Column(children: [
+            Container(
+              width: 500,
+              height: 300,
+              child: Landing(),
+            ),
+            Container(
+              height: 300,
+              child: SearchBar(),
+            ),
+          ]),
+        ));
+  }
+}
+
+class Test extends StatelessWidget {
+  const Test({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
